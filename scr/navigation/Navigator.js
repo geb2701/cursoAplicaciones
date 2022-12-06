be7a4172175500393c38fa9b339cb2f704b099b4
geb2701 <86382687+geb2701@users.noreply.github.com>
2022-12-05 21:51:00 -0300

    Tab navigation

---
+++ scr/navigation/Navigator.js
@@ -0,0 +1,49 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Game1 from "../Screens/Game1";
+import Game2 from "../Screens/Game2";
+import SelectGame from "../Screens/SelectGame";
+import IdeaShow from "../Screens/IdeaShow";
+
+const Stack = createNativeStackNavigator();
+
+export default Navigator = () => {
+  return (
+      <Stack.Navigator
+        initialRouteName="Categories"
+      >
+       <Stack.Screen
+          name="Categories"
+          component={SelectGame}
+          options={{
+            title: "Select Game",
+          }}
+        />
+
+        <Stack.Screen
+          name="Juego1"
+          component={Game1}
+        />
+
+        <Stack.Screen 
+          name="Juego2" 
+          component={Game2} 
+        />
+
+        <Stack.Screen 
+          name="Idea" 
+          component={IdeaShow}
+          options={({ route }) => ({
+            title: route.params.name,
+          })}
+        />
+
+        <Stack.Screen 
+          name="IdeaDescription" 
+          component={IdeaShow}
+          options={({ route }) => ({
+            title: route.params.name,
+          })}
+        />
+      </Stack.Navigator>
+  );
+}
